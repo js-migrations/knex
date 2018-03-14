c8a6ee3be4b50ea78a4fff2fc7c19aa7042dc735
Ryan Smith <0ryansmith1994@gmail.com>
2018-03-14 11:43:51 +0000

    feat: Changes migrations to an array instead of a dictionary.

--- src/FactoryConfig.ts
+++ src/FactoryConfig.ts
@@ -1,9 +1,9 @@
-import MigrationDictionary from '@js-migrations/core/dist/utils/types/MigrationDictionary';
+import Migration from '@js-migrations/core/dist/utils/types/Migration';
 import * as knex from 'knex';
 
 export default interface FactoryConfig {
   readonly db: () => Promise<knex>;
   readonly lockTableName?: string;
-  readonly migrations?: MigrationDictionary;
+  readonly migrations?: Migration[];
   readonly tableName?: string;
 }
